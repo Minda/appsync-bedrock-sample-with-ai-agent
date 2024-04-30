ec54905b9ffa7117d1ceadd6d4cf331e733ba164
Minda Myers <minda@hylo.com>
2024-04-30 10:52:04 -0700

    unstable transcription job works

--- handler-claude-simple/index.py
+++ handler-claude-simple/index.py
@@ -3,26 +3,40 @@ from chatResponder import ChatResponder
 from botocore.config import Config
 
 bedrock = boto3.client('bedrock-runtime', config=Config(region_name='us-east-1'))
-s3_client = boto3.client('s3')
-transcribe_client = boto3.client('transcribe')
+s3_client = boto3.client('s3', config=Config(region_name='us-east-1'))
+transcribe_client = boto3.client('transcribe', config=Config(region_name='us-east-1'))
 logging.basicConfig(level=logging.INFO)
 logging.getLogger().setLevel(logging.INFO)
 
 def transcribe_audio(audio_url):
-
     print("transcribe audio... ")
-    print(audio_url)
+    print("audio url", audio_url)
+    logging.info(f"audio url: {audio_url}")
 
     # Extract the bucket and key from the audio URL
     bucket, key = audio_url.replace("https://s3.amazonaws.com/", "").split("/", 1)
 
     # Generate a unique job name
     job_name = f"transcribe-job-{int(time.time())}"
+    logging.info(f"job_name: {job_name}")
+
+    # Remove leading/trailing whitespace and single quotes from the audio URL
+    audio_url = audio_url.strip().strip("'")
+
+    bucket_name = 'aws-audio-recordings'
+    file_name = audio_url.split('/')[-1] #get the last element
+
+    # deeplearning.io notebook version of file url
+    media_file_uri = f's3://{bucket_name}/{file_name}'
+    logging.info(f"bucket_name: {bucket_name}")
+    logging.info(f"file_name: {file_name}")
+    logging.info(f"media_file_uri: {media_file_uri}")
 
     # Start the transcription job
     response = transcribe_client.start_transcription_job(
         TranscriptionJobName=job_name,
         Media={'MediaFileUri': audio_url},
+        #Media={'MediaFileUri': media_file_uri},
         MediaFormat='webm',
         LanguageCode='en-US'
     )
@@ -39,6 +53,7 @@ def transcribe_audio(audio_url):
         transcript_file_uri = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
         transcript_response = s3_client.get_object(Bucket='aws-audio-recordings', Key=transcript_file_uri.split("/")[-1])
         transcript = json.loads(transcript_response['Body'].read().decode('utf-8'))
+        logging.info(transcript)
         return transcript['results']['transcripts'][0]['transcript']
     else:
         raise Exception("Transcription job failed.")
@@ -71,6 +86,8 @@ def handler(event, context):
     try:
         logging.info("This is an informational message")
         logging.error("This was Minda and Vlad making an catwifhat message")
+        logging.info(event)
+        #logging.info(event['audioFileUrl'])
 
         chatResponder.publish_agent_message("activated handler")
         # Get the audio URL from the event
@@ -82,6 +99,7 @@ def handler(event, context):
 
         # Transcribe the audio using Amazon Transcribe
         transcribed_text = transcribe_audio(audio_url)
+        logging.info(transcribed_text)
 
         chatResponder.publish_agent_message(transcribed_text)
 
