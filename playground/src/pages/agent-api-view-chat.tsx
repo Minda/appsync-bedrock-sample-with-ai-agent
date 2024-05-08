08ccbfef751bd957af8d28a5f8210cf598dae1d3
Minda Myers <minda@hylo.com>
2024-05-08 12:30:18 -0700

    Added a UI component for language selection TODO: pass language to API

--- playground/src/pages/agent-api-view-chat.tsx
+++ playground/src/pages/agent-api-view-chat.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom"
 import { useAgentApiAgent, useAgentApiSendMessage, useAgentApiSubscribeConversation } from "../apis/agent-api"
-import { Loader, TextAreaField, View, Flex, Card } from "@aws-amplify/ui-react"
+import { Loader, TextAreaField, View, Flex, Card, SelectField } from "@aws-amplify/ui-react" //ui.docs.amplify.aws/react
 import { Container } from "../library/container"
 import { ChatRendered } from "../library/chat/chat-rendered"
 import { useEffect, useState } from "react"
@@ -23,6 +23,9 @@ export function AIAgentViewChat () {
     const resetMetadata = useResetAgentConversationMetadata()
     const submitMessage = useAgentApiSendMessage(chatId)
     useAgentApiSubscribeConversation(chatId)
+
+    const [languageIn, setLanguageIn] = useState("English");
+    const [languageOut, setLanguageOut] = useState("French");
 
     //TODO: Add a lanuage selector
     //https://ui.docs.amplify.aws/react/components/selectfield
@@ -47,19 +50,55 @@ export function AIAgentViewChat () {
       submitMessage({ message: chatString, audioFileUrl: audioUrl });
     };
 
+    /* ... other components */
     return (
         <Flex>
             <View width={900}>
                 <Container heading={`Chatting with '${agentObject.value.name}'`} minHeight={500} padBody={0}>
                     <ChatRendered/>
                 </Container>
-                <AudioRecorder onRecordingComplete={handleRecordingComplete}/>
+                <Card>
+                    <Flex direction={'row'} alignContent={'center'} >
+                        <View width={'50%'}>
+                            <SelectField
+                              label="Language In"
+                              value={languageIn}
+                              onChange={(e) => setLanguageIn(e.target.value)}
+                            >
+                                <option value="English">English</option>
+                                <option value="French">French</option>
+                                <option value="Spanish">Spanish</option>
+                                <option value="German">German</option>
+                                <option value="Italian">Italian</option>
+                                <option value="Ukrainian">Ukrainian</option>
+                            </SelectField>
+                            <SelectField
+                                label="Language Out"
+                                value={languageOut}
+                                onChange={(e) => setLanguageOut(e.target.value)}
+                            >
+                                <option value="English">English</option>
+                                <option value="French">French</option>
+                                <option value="Spanish">Spanish</option>
+                                <option value="German">German</option>
+                                <option value="Italian">Italian</option>
+                                <option value="Ukrainian">Ukrainian</option>
+                            </SelectField>
+                        </View>
+                        <View >
+                            <AudioRecorder onRecordingComplete={handleRecordingComplete}/>
+                        </View>
+
+
+                    </Flex>
+                </Card>
+
                 <Card>
                     {
                         conversationMetadata.responding && <Loader variation="linear"/>
                     }
                     {
-                        !conversationMetadata.responding && <TextAreaField 
+                        !conversationMetadata.responding && <TextAreaField
                             labelHidden
                             label="Message"
                             placeholder="Type your message here"
